1d7d8b56344e901085667400887d5ed4d512b2d7
Faraz <farazmoin.uk@gmail.com>
2026-04-29 19:45:49 +0530

    Initial commit7

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-genai.configure(api_key=st.secrets["PASS"])
+genai.configure(api_key="PASS")
 gemini_model = genai.GenerativeModel('models/gemini-3.1-flash-lite-preview')
 st.title("YF Assistant")
 if "chat_history" not in st.session_state:
